10d78259fdd828698cfc203009ce1a973f8f94fd
Samuel Kutz <117867055+samuelkutz@users.noreply.github.com>
2023-08-02 21:39:38 -0300

    correções de identação no script

--- library.sql
+++ library.sql
@@ -1,58 +1,59 @@
 CREATE DATABASE IF NOT EXISTS Library;
 #
 # Declarando as entidades
+#	
 CREATE TABLE IF NOT EXISTS category(
 	id int NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
-    
-    PRIMARY KEY (id)
+	name varchar(50) NOT NULL,
+	
+    	PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS author(
 	id int NOT NULL AUTO_INCREMENT,
-    name varchar(50),
-    
-    PRIMARY KEY (id)
+    	name varchar(50),
+	
+    	PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS publisher(
 	id int NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
-    
+    	name varchar(50) NOT NULL,
+	
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS student(
 	id int NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
-    
-    PRIMARY KEY (id)
+    	name varchar(50) NOT NULL,
+	
+    	PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS book(
 	id int NOT NULL AUTO_INCREMENT,
-    title varchar(50) NOT NULL,
+    	title varchar(50) NOT NULL,
 	year_published int NOT NULL,
 	author_id int NOT NULL,
-    category_id int NOT NULL, 
+    	category_id int NOT NULL, 
 	publisher_id int NOT NULL,
     
-    PRIMARY KEY (id),
+    	PRIMARY KEY (id),
 	FOREIGN KEY (author_id) REFERENCES author(id),
-    FOREIGN KEY (category_id) REFERENCES category(id),
+    	FOREIGN KEY (category_id) REFERENCES category(id),
 	FOREIGN KEY (publisher_id) REFERENCES publisher(id)
 );
 
 CREATE TABLE IF NOT EXISTS transactions(
 	id int NOT NULL AUTO_INCREMENT,
-    student_id int NOT NULL,
+    	student_id int NOT NULL,
 	book_id int NOT NULL,
-    taken_date date NOT NULL,
-    brought_date date NOT NULL,
+    	taken_date date NOT NULL,
+    	brought_date date NOT NULL,
     
-    PRIMARY KEY (id),
-    FOREIGN KEY (book_id) REFERENCES book(id),
-    FOREIGN KEY (student_id) REFERENCES student(id)
+    	PRIMARY KEY (id),
+    	FOREIGN KEY (book_id) REFERENCES book(id),
+   	 FOREIGN KEY (student_id) REFERENCES student(id)
 );
 #
 # Populando as tabelas :D
